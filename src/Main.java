b08d9cfc1903af2703e9a08d0a7b43db19d113e1
irfan <irfan.bouhenaf@etu.univ-amu.fr>
2022-01-05 04:29:30 +0100

    test

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+//!\mainpage Projet Java - Groupe 1
+//!
+//!\section intro_section Présentation
+//! Un logiciel permettant de gérer ses albums de musiques!
+//!\section how_to_use Comment l'utiliser ?
+//!\ Placez vous dans le répertoire où se trouvent les fichiers java et éxécutez la commande "java -classpath ".;sqlite-jdbc-3.30.1.jar" Main"
+//!\section pour_javadoc Pour avoir la documentation style javadoc :
+//!        Placer vous dans le répertoire précédant celui où se trouvent les fichiers java et éxécutez la commande "javadoc -d -encoding utf8 -docencoding utf8 -charset utf8 doc src/*"
+//!        L'encoding utf8 permettra d'afficher les accents dans la javadoc, ce qui est quand même un grand plus
+//!\section thank_you Merci d'avoir lu !
+//!\section author_section Liste des auteurs
+//!    BOUHENAF Irfan et MOLINA Romain
+
+
+public class Main 
+{
+    public static void main (String [] arg)
+    {
+        Fenetre fenetre = new Fenetre();
+    }
+    
+}
